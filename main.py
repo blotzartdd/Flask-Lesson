adec0dfc66a7db7b4b126542d1a7b8149348d344
blotzartdd <88193010+blotzartdd@users.noreply.github.com>
2022-02-03 16:55:03 +0300

    Promotion Image

---
+++ main.py
@@ -0,0 +1,91 @@
+from flask import Flask, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def start():
+    return 'Миссия Колонизация Марса'
+
+
+@app.route('/index')
+def index():
+    return "И на Марсе будут яблони цвести!"
+
+
+@app.route('/promotion')
+def promotion():
+    promotion_list = ['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
+                      'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!', 'Присоединяйся!']
+    return '</br>'.join(promotion_list)
+
+
+@app.route('/sample_page')
+def return_sample_page():
+    return """<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <title>Привет, Яндекс!</title>
+                  </head>
+                  <body>
+                    <h1>Первая HTML-страница</h1>
+                  </body>
+                </html>"""
+
+
+@app.route('/image_mars')
+def image_mars():
+    return f"""<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <title>Привет, Марс!</title>
+                      </head>
+                      <body>
+                        <h1>Жди нас, Марс!</h1>
+                        <img src="{url_for('static', filename='img/download.jpg')}">
+                        <br>Вот она какая, красная планета.
+                      </body>
+                    </html>"""
+
+
+@app.route('/promotion_image')
+def promotion_image():
+    promotion_list = ['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
+                      'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!', 'Присоединяйся!']
+    return f"""<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                        <title>Колонизация</title>
+                      </head>
+                      <body>
+                        <h1>Жди нас, Марс!</h1>
+                        <img src="{url_for('static', filename='img/download.jpg')}">
+                        <div class="alert alert-dark" role="alert">
+                            {promotion_list[0]}
+                        </div>
+                        <div class="alert alert-success" role="alert">
+                            {promotion_list[1]}
+                        </div>
+                        <div class="alert alert-secondary" role="alert">
+                            {promotion_list[2]}
+                        </div>
+                        <div class="alert alert-warning" role="alert">
+                            {promotion_list[3]}
+                        </div>
+                        <div class="alert alert-danger" role="alert">
+                            {promotion_list[4]}
+                        </div>
+                      </body>
+                    </html>"""
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
